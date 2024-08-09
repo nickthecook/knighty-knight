1197f5428481dafbd9a81c070767789eddcaa893
nickthecook <nickthecook@gmail.com>
2024-08-09 13:15:46 -0400

    add start message

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,13 +5,19 @@ var lives = 3
 var checkpoint_x = -39
 var checkpoint_y = 12
 
-@onready var score_label = $UI/ScoreLabel
-@onready var game_over_label = $UI/GameOverLabel
 @onready var player = $"../Player"
-@onready var timer = $Timer
-@onready var sleep_timer = $SleepTimer
 @onready var killzone = $"../Killzone"
+
+@onready var death_timer = $DeathTimer
+@onready var sleep_timer = $SleepTimer
+@onready var so_tired_timer = $SoTiredTimer
+@onready var so_tired_start_timer = $SoTiredStartTimer
+
+@onready var game_over_label = $UI/GameOverLabel
+@onready var score_label = $UI/ScoreLabel
 @onready var lives_label = $UI/LivesLabel
+@onready var so_tired_label = $UI/SoTiredLabel
+
 @onready var bed = $"../Checkpoints/Bed"
 @onready var bed_2 = $"../Checkpoints/Bed2"
 @onready var bed_3 = $"../Checkpoints/Bed3"
@@ -24,6 +30,7 @@ func _ready():
 	bed_2.player_sleeping.connect(player_sleeping)
 	bed_3.player_sleeping.connect(player_sleeping)
 	bed_4.player_sleeping.connect(player_sleeping)
+	so_tired_start_timer.start()
 
 func player_sleeping(checkpoint):
 	if checkpoint_x == checkpoint.position.x and checkpoint_y == checkpoint.position.y:
@@ -53,13 +60,13 @@ func handle_game_over():
 	print("Game over.")
 	Engine.time_scale = 0.5
 	player.die()
-	timer.start()
+	death_timer.start()
 
 func handle_death():
 	print("You died.")
 	Engine.time_scale = 0.5
 	player.die()
-	timer.start()
+	death_timer.start()
 
 func _on_timer_timeout():
 	Engine.time_scale = 1
@@ -70,3 +77,14 @@ func _on_timer_timeout():
 
 func _on_sleep_timer_timeout():
 	player.wake()
+
+
+func _on_so_tired_start_timer_timeout():
+	so_tired_label.visible = true
+	so_tired_timer.start()
+	
+
+
+
+func _on_so_tired_timer_timeout():
+	so_tired_label.visible = false
